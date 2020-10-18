8f7964af7349697c988ab6ffa028c04b7952449f
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-10-18 17:02:10 +0200

    Changed App.js structure and put ContactList instead Contact components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from "react";
-import Contact from "./components/Contact";
+import ContactList from './components/ContactList';
 
 export default function App() {
   return (
     <div>
-      <Contact
-        name="Aaron Davis"
-        avatar="https://randomuser.me/api/portraits/men/88.jpg"
-        online
-        statustext="online"
-      />
-      <Contact
-        name="Diane Butler"
-        avatar="https://randomuser.me/api/portraits/women/56.jpg"
-        online={false}
-        statustext="offline"
-      />
-      <Contact
-        name="Tim Brewer"
-        avatar="https://randomuser.me/api/portraits/men/39.jpg"
-        online
-        statustext="online"
-      />
+      <ContactList/>
     </div>
   );
 }
