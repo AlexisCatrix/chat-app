485c5cfba79aed9e2dcb1b57b073608a735bf0c5
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-10-19 00:08:38 +0200

    Changed Contact fucntion by Contact Class and give a state for a dynamic chat

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,21 +2,44 @@ import PropTypes from "prop-types";
 import React from "react";
 import "./Contact.css";
 
-export default function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name}></img>
-      <div>
-        <div className="name">{props.name}</div>
-        <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"}>
-            {props.status}
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Contact">
+        <img
+          className="avatar"
+          src={this.props.avatar}
+          alt={this.props.name}
+        ></img>
+        <div>
+          <div className="name">{this.props.name}</div>
+          <div
+            className="status"
+            onClick={(event) => {
+              const changeStatus = !this.state.online;
+              this.setState({ online: changeStatus });
+            }}
+          >
+            <div
+              className={this.state.online ? "status-online" : "status-offline"}
+            >
+              {this.state.status}
+            </div>
+            <div className="status-text">
+              {this.state.online ? "online" : "offline"}
+            </div>
           </div>
-          <div className="status-text">{props.online ? "online" : "offline"}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Contact.propTypes = {
